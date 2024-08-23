906de9250eda41a53d1f49a783691c3da2675ad8
Amber Jain <jainanuamber@gmail.com>
2024-08-23 17:25:55 +0530

    added kapt dependency for databinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
     }
     viewBinding.enable = true
 }
