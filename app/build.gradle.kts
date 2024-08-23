f7c176bae8c25dea0fb96f4416aa834e70dd4fd6
Amber Jain <jainanuamber@gmail.com>
2024-08-23 17:05:15 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-//    viewBinding.enable = true
+    viewBinding.enable = true
 }
 
 dependencies {
--- app/src/main/java/com/huddle01/kotlin_client_example_app/Application.kt
+++ app/src/main/java/com/huddle01/kotlin_client_example_app/Application.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.huddle01.kotlin_client.HuddleClient
 
 class Application : Application() {
-    private lateinit var huddleClient: HuddleClient
+    lateinit var huddleClient: HuddleClient
     override fun onCreate() {
         super.onCreate()
         huddleClient = HuddleClient("YOUR_PROJECT_ID", this)
