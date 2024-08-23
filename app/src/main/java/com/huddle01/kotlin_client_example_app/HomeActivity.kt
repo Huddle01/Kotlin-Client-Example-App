edaa79cae321add75c0595bbca47e55a7fc440ae
Amber Jain <jainanuamber@gmail.com>
2024-08-23 17:05:27 +0530

    updated HomeActivity.kt ✅

--- app/src/main/java/com/huddle01/kotlin_client_example_app/HomeActivity.kt
+++ app/src/main/java/com/huddle01/kotlin_client_example_app/HomeActivity.kt
@@ -1,20 +1,38 @@
 package com.huddle01.kotlin_client_example_app
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.huddle01.kotlin_client_example_app.databinding.ActivityHomeBinding
+import kotlinx.coroutines.launch
+
 
 class HomeActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        window.statusBarColor = ContextCompat.getColor(this, R.color.trans_bg)
+        setContentView(binding.root)
+        val huddleClient = (applicationContext as Application).huddleClient
+
+        binding.btnConnect.setOnClickListener {
+            val roomId = binding.roomId.text.toString().trim()
+            val token = binding.token.text.toString().trim()
+
+            lifecycleScope.launch {
+                try {
+                    huddleClient.joinRoom(roomId, token)
+                    val intent = Intent(this@HomeActivity, MeetingRoomActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    Toast.makeText(this@HomeActivity, "Failed to join the room: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
