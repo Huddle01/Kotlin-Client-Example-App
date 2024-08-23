8809111e31c5226a93da75cb9952c173e6821c6f
Amber Jain <jainanuamber@gmail.com>
2024-08-23 16:55:00 +0530

    deleted MainActivity.kt & added Application.kt

---
+++ app/src/main/java/com/huddle01/kotlin_client_example_app/Application.kt
@@ -0,0 +1,11 @@
+package com.huddle01.kotlin_client_example_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Application : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
