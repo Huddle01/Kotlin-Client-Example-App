321e961b65af0c6004ce45662844b44eaf959962
Amber Jain <jainanuamber@gmail.com>
2024-10-17 21:43:06 +0530

    added LiveRoomChatActivity

---
+++ app/src/main/java/com/huddle01/kotlin_client_example_app/LiveRoomChatActivity.kt
@@ -0,0 +1,20 @@
+package com.huddle01.kotlin_client_example_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LiveRoomChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_live_room_chat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
