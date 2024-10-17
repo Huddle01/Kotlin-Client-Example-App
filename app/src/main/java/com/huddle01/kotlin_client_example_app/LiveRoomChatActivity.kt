2f1b6da669db4893ef0fd0fa118f6f5f2fd91f4f
Amber Jain <jainanuamber@gmail.com>
2024-10-17 22:24:57 +0530

    updated LiveRoomChatActivity

--- app/src/main/java/com/huddle01/kotlin_client_example_app/LiveRoomChatActivity.kt
+++ app/src/main/java/com/huddle01/kotlin_client_example_app/LiveRoomChatActivity.kt
@@ -3,18 +3,16 @@ package com.huddle01.kotlin_client_example_app
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LiveRoomChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        window.statusBarColor = ContextCompat.getColor(this, R.color.trans_bg)
+
         setContentView(R.layout.activity_live_room_chat)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
