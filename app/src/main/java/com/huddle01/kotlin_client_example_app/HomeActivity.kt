9ae363ab25955f67f52bed88c166693d09c79ca7
Amber Jain <jainanuamber@gmail.com>
2024-10-18 12:40:09 +0530

    updated HomeActivity.kt

--- app/src/main/java/com/huddle01/kotlin_client_example_app/HomeActivity.kt
+++ app/src/main/java/com/huddle01/kotlin_client_example_app/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.huddle01.kotlin_client.models.enum_class.RoomStates
 import com.huddle01.kotlin_client_example_app.databinding.ActivityHomeBinding
 import kotlinx.coroutines.launch
 
@@ -18,19 +19,34 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.statusBarColor = ContextCompat.getColor(this, R.color.trans_bg)
         setContentView(binding.root)
+
         val huddleClient = (applicationContext as Application).huddleClient
+        val store = huddleClient.localPeer.store
+
+        store.roomInfo.observe(this) { roomInfo ->
+            if (roomInfo.connectionState == RoomStates.CONNECTED) {
+                val intent = Intent(this@HomeActivity, LiveRoomChatActivity::class.java).apply {
+                    putExtra("isMicOn", binding.switchMic.isChecked)
+                    putExtra("isCamOn", binding.switchCamera.isChecked)
+                }
+                startActivity(intent)
+            }
+        }
 
         binding.btnConnect.setOnClickListener {
-            val roomId = binding.roomId.text.toString().trim()
-            val token = binding.token.text.toString().trim()
+
+            val roomId = binding.roomId.text.toString()
+            val token = binding.token.text.toString()
 
             lifecycleScope.launch {
                 try {
                     huddleClient.joinRoom(roomId, token)
-                    val intent = Intent(this@HomeActivity, MeetingRoomActivity::class.java)
-                    startActivity(intent)
                 } catch (e: Exception) {
-                    Toast.makeText(this@HomeActivity, "Failed to join the room: ${e.message}", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this@HomeActivity,
+                        "Failed to join the room: ${e.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
         }
